9a2b318c2626559157dd84ef88e40014522f79e5
digeut <121070111+dohyeon0206@users.noreply.github.com>
2024-02-18 00:08:37 +0900

    gkxm

--- Assets/01.Scripts/FadeScript.cs
+++ Assets/01.Scripts/FadeScript.cs
@@ -18,10 +18,11 @@ public class FadeScript : MonoBehaviour
     {
         Panel.gameObject.SetActive(true);
         Color alpha = Panel.color;
-        while (alpha.a < 1f)
+        while (alpha.a < 225f)
         {
             time += Time.deltaTime / F_time;
             alpha.a = Mathf.Lerp(0, 1, time);
+            Panel.color = alpha;
             yield return null;
         }
         yield return null;
