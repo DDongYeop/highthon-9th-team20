f682b8e53327a86b2b801524c29c055c1ede44b4
digeut <121070111+dohyeon0206@users.noreply.github.com>
2024-02-18 02:10:12 +0900

    rlxkemdemd

---
+++ Assets/01.Scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Tutorial : MonoBehaviour
+{
+    [Header("Loading Text")]
+    [SerializeField] private TMP_Text loadingText_Title;
+
+    [Header("Loading Text")]
+    [SerializeField] private TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
