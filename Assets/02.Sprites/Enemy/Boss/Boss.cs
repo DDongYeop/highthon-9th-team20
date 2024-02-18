2c13e19648b7fdbf798709841655d03494d11cb2
JunghyunByun <bjh1206408@gmail.com>
2024-02-18 11:02:03 +0900

    df

--- Assets/02.Sprites/Enemy/Boss/Boss.cs
+++ Assets/02.Sprites/Enemy/Boss/Boss.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private GameObject targetPosition;
+    private Animator animator;
+    private bool walk = true;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        targetPosition = GameObject.Find("Player").gameObject;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x > 0 || transform.position.x < 0)
+            animator.SetBool("IsWalk", true);
+        else
+        {
+            animator.SetBool("IsWalk", false);
+        }
+
+        if (transform.position.y <= -5f)
+        {
+            transform.position = new Vector3(transform.position.x, -5f, transform.position.z);
+        }
+            
+
+        if (walk != false)
+        {
+            walk = false;
+
+            StartCoroutine(BossWalk());
+        } 
+    }
+
+    IEnumerator BossWalk()
+    {
+        yield return new WaitForSeconds(0.15f);
+        walk = true;
+
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition.transform.position, 2f);
     }
 }
